90e2b914424747c0185fa9f1a0c0b50e0cf7722a
David Moina <davemolina777@gmail.com>
2020-10-28 16:42:22 -0500

    commit tree 51230fd9cf0e498fce46114b6ce7f2d48fe2c678

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    messages = []
+    attendees.each do |badges| 
+     messages.push  "Hello, my name is #{badges}."
+    end
+  return messages
+end
+
+def assign_rooms(array)
+    assigned = []
+    num = 1
+    array.each do |rooms|
+    assigned.push "Hello, #{rooms}! You'll be assigned to room #{num}!"
+    num += 1
+    end
+    return assigned
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+        puts badge
+    end
+    assign_rooms(array).each do |badge|
+        puts badge
+    end
+end
